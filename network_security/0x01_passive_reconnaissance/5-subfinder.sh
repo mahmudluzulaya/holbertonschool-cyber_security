7fc9d91b227d3c6332c7acdc0b013bad5390c1e3
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-12-28 11:56:17 +0000

    jdjd

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -silent -d "$1" | tee /tmp/subs.txt && while read s; do ip=$(dig +short "$s" | head -n1); [ -n "$ip" ] && echo "$s,$ip"; done < /tmp/subs.txt > "$1.txt"
+subfinder -silent -nW -oI -d "$1" -o "$1.txt"
