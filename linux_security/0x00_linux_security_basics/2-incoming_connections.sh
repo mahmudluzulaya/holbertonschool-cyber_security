04d21708297d092827c3fab1f438a0c2086fd461
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-12-28 10:48:42 +0000

    jdjj

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo last -n 5 -F
+# Run as privileged user
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp -j DROP
+sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo ip6tables -A INPUT -p tcp -j DROP
+echo "Rules updated"
+echo "Rules updated (v6)"
