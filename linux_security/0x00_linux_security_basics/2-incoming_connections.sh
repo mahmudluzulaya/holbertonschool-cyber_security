65263de208da4eb5e563e4f62139decb4d923de7
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-12-28 10:50:05 +0000

    jdjj

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# Run as privileged user
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -p tcp -j DROP
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo ip6tables -A INPUT -p tcp -j DROP
-echo "Rules updated"
-echo "Rules updated (v6)"
+sudo ufw allow 80/tcp
