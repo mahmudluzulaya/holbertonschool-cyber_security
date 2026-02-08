b917fdce97c51802ea77d721c125181605b93f1f
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-02-08 18:46:41 +0000

    kddj

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && echo "<hashed_passwords>" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && echo "Password123" > 4-password.txt
