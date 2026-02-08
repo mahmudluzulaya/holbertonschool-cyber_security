dd18887f069d58242b690640d4a3dded9a16eaa9
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-02-08 18:31:48 +0000

    jfjfj

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if a password argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 \"password\""
-    exit 1
-fi
-
-# Hash the password using SHA-1 and store it in 0_hash.txt
-echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
+echo -n "$1" | sha1sum > 0_hash.txt
