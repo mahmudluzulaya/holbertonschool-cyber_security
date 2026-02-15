613bcceae5c4a5414f1526b78617df2282556f8c
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-02-15 18:21:58 +0000

    fj

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if ! grep -q "smtpd_tls_security_level" /etc/postfix/main.cf; then
-    echo "STARTTLS not configured"
-else
-    grep "smtpd_tls_security_level" /etc/postfix/main.cf
-fi
+grep "smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
