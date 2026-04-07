9c59fd65b961f08c2ef647d9c41940ddd4714174
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-04-07 13:16:26 +0000

    hjf

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ATTACKER_IP=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -rn | head -1 | awk '{print $2}')
-grep "$ATTACKER_IP" logs.txt | awk -F'\"' '{print $6}' | sort | uniq -c | sort -rn | head -1 | awk '{print $1}'
+ATTACKER_IP=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+grep "^$ATTACKER_IP" logs.txt | awk -F\" '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
