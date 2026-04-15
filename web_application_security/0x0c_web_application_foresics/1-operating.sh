a0c0e20da97a7ba479a418c15a18e2699e16d43d
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-04-15 13:25:22 +0000

    this is a commit

--- web_application_security/0x0c_web_application_foresics/1-operating.sh
+++ web_application_security/0x0c_web_application_foresics/1-operating.sh
@@ -1,12 +1,2 @@
-#!/bin/bash
-# This script extracts the OS/Kernel version from the dmesg log file
-
-LOG_FILE="dmseg"
-
-if [ ! -f "$LOG_FILE" ]; then
-    echo "Log file $LOG_FILE not found."
-    exit 1
-fi
-
-# Search for the string "Linux version" within the file
-grep "Linux version" "$LOG_FILE"
+~ #!/bin/bash
+grep "Linux version" dmesg
