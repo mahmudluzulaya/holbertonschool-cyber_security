c793d7548a4bc008237e42108cf92ae159c66a4f
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-05-24 14:03:42 -0400

    djjd

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -3,25 +3,29 @@ require 'uri'
 require 'json'
 
 def post_request(url, body_params = {})
-    uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-        
-        if uri.scheme == 'https'
-              http.use_ssl = true
-                end
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  
+  if uri.scheme == 'https'
+    http.use_ssl = true
+  end
 
-          request = Net::HTTP::Post.new(uri.path.empty? ? '/' : uri.path, { 'Content-Type' => 'application/json' })
-            request.body = body_params.to_json
+  request = Net::HTTP::Post.new(uri.path.empty? ? '/' : uri.path, { 'Content-Type' => 'application/json' })
+  request.body = body_params.to_json
 
-              response = http.request(request)
+  response = http.request(request)
 
-                puts "Response status: #{response.code} #{response.message}"
-                  puts "Response body:"
-                    
-                    begin
-                          parsed = JSON.parse(response.body)
-                              puts JSON.pretty_generate(parsed)
-                                rescue
-                                      puts response.body
-                                        end
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  
+  begin
+    parsed = JSON.parse(response.body)
+    if parsed.empty?
+      puts "{}"
+    else
+      puts JSON.pretty_generate(parsed)
+    end
+  rescue
+    puts response.body
+  end
 end
