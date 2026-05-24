e76f2826ea5ef477544634f153f43402828a7d43
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-05-24 14:08:08 -0400

    djjd

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,31 @@
+require 'digest'
+
+if ARGV.length != 2
+  puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
+  exit
+end
+
+target_hash = ARGV[0].downcase
+dictionary_file = ARGV[1]
+
+unless File.exist?(dictionary_file)
+  exit
+end
+
+password_found = nil
+
+File.foreach(dictionary_file) do |line|
+  word = line.strip
+  word_hash = Digest::SHA256.hexdigest(word)
+
+  if word_hash == target_hash
+    password_found = word
+    break
+  end
+end
+
+if password_found
+  puts "Password found: #{password_found}"
+else
+  puts "Password not found in dictionary."
+end
