ad7e6c7969ffbb686527d172d24df56f17f2addd
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-05-24 13:50:08 -0400

    djjd

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,4 +1,4 @@
-re 'json'
+require 'json'
 
 def merge_json_files(file1_path, file2_path)
     return unless File.exist?(file1_path) && File.exist?(file2_path)
@@ -14,4 +14,6 @@ def merge_json_files(file1_path, file2_path)
                 File.open(file2_path, 'w') do |f|
                       f.write(JSON.pretty_generate(merged_data))
                         end
+
+                  puts "Merged JSON written to #{file2_path}"
 end
