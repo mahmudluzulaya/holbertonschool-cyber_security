292af0cdae03ac0d5eca49be11763be35772606e
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-05-24 14:06:44 -0400

    djjd

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -1,10 +1,10 @@
-re 'open-uri'
+require 'open-uri'
 require 'uri'
 require 'fileutils'
 
 if ARGV.length != 2
-    puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
-      exit
+  puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
+  exit
 end
 
 url = ARGV[0]
@@ -13,15 +13,15 @@ local_path = ARGV[1]
 puts "Downloading file from #{url}..."
 
 begin
-    URI.open(url) do |remote_file|
-          dirname = File.dirname(local_path)
-              FileUtils.mkdir_p(dirname) unless Dir.exist?(dirname)
+  URI.open(url) do |remote_file|
+    dirname = File.dirname(local_path)
+    FileUtils.mkdir_p(dirname) unless Dir.exist?(dirname)
 
-                  File.open(local_path, 'wb') do |local_file|
-                          local_file.write(remote_file.read)
-                              end
-                    end
-      puts "File downloaded and saved to #{local_path}."
+    File.open(local_path, 'wb') do |local_file|
+      local_file.write(remote_file.read)
+    end
+  end
+  puts "File downloaded and saved to #{local_path}."
 rescue => e
-    puts "Error: #{e.message}"
+  puts "Error: #{e.message}"
 end
