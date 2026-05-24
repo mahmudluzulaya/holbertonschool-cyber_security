3747adf1e6ac1b2965066d8317ea584bd98bf4bc
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-05-24 14:00:05 -0400

    djjd

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,9 +1,10 @@
 def print_arguments
-    if ARGV.empty?
-          puts "No arguments provided."
-            else
-                  ARGV.each_with_index do |arg, index|
-                          puts "#{index + 1}. #{arg}"
-                              end
-                    end
+  if ARGV.empty?
+    puts "No arguments provided."
+  else
+    puts "Arguments:"
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
+    end
+  end
 end
