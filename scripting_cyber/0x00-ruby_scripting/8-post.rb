649dd346f0d58cbbbd5ae06d1e1d7065d29a6261
mahmudluzulaya <mahmudovazulaye@gmail.com>
2026-05-24 14:01:56 -0400

    djjd

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,27 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+def post_request(url, body_params = {})
+    uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+        
+        if uri.scheme == 'https'
+              http.use_ssl = true
+                end
+
+          request = Net::HTTP::Post.new(uri.path.empty? ? '/' : uri.path, { 'Content-Type' => 'application/json' })
+            request.body = body_params.to_json
+
+              response = http.request(request)
+
+                puts "Response status: #{response.code} #{response.message}"
+                  puts "Response body:"
+                    
+                    begin
+                          parsed = JSON.parse(response.body)
+                              puts JSON.pretty_generate(parsed)
+                                rescue
+                                      puts response.body
+                                        end
+end
